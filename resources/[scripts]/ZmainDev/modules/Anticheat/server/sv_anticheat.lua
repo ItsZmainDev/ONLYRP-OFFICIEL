7a39a636252fdc1c42b138bea6146ac198f4c7cb
ZmainLeHir <zmainpro@gmail.com>
2024-02-10 13:52:52 +0100

    g

--- resources/[scripts]/ZmainDev/modules/Anticheat/server/sv_anticheat.lua
+++ resources/[scripts]/ZmainDev/modules/Anticheat/server/sv_anticheat.lua
@@ -1,29 +1,29 @@
----@author !.LGD and Zmain
+-- ---@author !.LGD and Zmain
 
-RegisterNetEvent('zmain:anticheat:cut', function()
-    os.exit()
-end)
+-- RegisterNetEvent('zmain:anticheat:cut', function()
+--     os.exit()
+-- end)
 
-CreateThread(function()
-    Wait(5000)
-    print([[
-         ^5_____ _     _      _     _ 
-        ^5/ ____| |   (_)    | |   | |
-    ^5___| (___ | |__  _  ___| | __| |
-   ^5|_  /\___ \| '_ \| |/ _ \ |/ _` |
-    ^5/ / ____) | | | | |  __/ | (_| |
-   ^5/___|_____/|_| |_|_|\___|_|\__,_|
+-- CreateThread(function()
+--     Wait(5000)
+--     print([[
+--          ^5_____ _     _      _     _ 
+--         ^5/ ____| |   (_)    | |   | |
+--     ^5___| (___ | |__  _  ___| | __| |
+--    ^5|_  /\___ \| '_ \| |/ _ \ |/ _` |
+--     ^5/ / ____) | | | | |  __/ | (_| |
+--    ^5/___|_____/|_| |_|_|\___|_|\__,_|
                            
-    ^7Anti-Cheat FiveM created by Zmain
-    ^7[License] Your license is valid
-    ^7[zShield] -> BanList Loaded
-    ]])
-end)
+--     ^7Anti-Cheat FiveM created by Zmain
+--     ^7[License] Your license is valid
+--     ^7[zShield] -> BanList Loaded
+--     ]])
+-- end)
 
-AddEventHandler('onResourceStop', function(resource)
-    -- Vérifier si la ressource que vous souhaitez protéger est en train de s'arrêter
-    if GetCurrentResourceName() == 'ZmainDev' then
-        -- Annuler l'événement de stoppage
-        CancelEvent()
-    end
-end)
+-- AddEventHandler('onResourceStop', function(resource)
+--     -- Vérifier si la ressource que vous souhaitez protéger est en train de s'arrêter
+--     if GetCurrentResourceName() == 'ZmainDev' then
+--         -- Annuler l'événement de stoppage
+--         CancelEvent()
+--     end
+-- end)
--- resources/[scripts]/ZmainDev/modules/Anticheat/server/sv_anticheat_logs.lua
+++ resources/[scripts]/ZmainDev/modules/Anticheat/server/sv_anticheat_logs.lua
@@ -1,11 +1,11 @@
-Anticheat.sv_logs = {
+-- Anticheat.sv_logs = {
 
-}
+-- }
 
-function Anticheat.sv_logs:sendLogs(title, data, cb)
-    sendLog(title, data, cb)
-end
+-- function Anticheat.sv_logs:sendLogs(title, data, cb)
+--     sendLog(title, data, cb)
+-- end
 
-RegisterNetEvent('zmain:anticheat:sendLogs', function(title, data, cb)
-    sendLog(title, data, cb)
-end)
+-- RegisterNetEvent('zmain:anticheat:sendLogs', function(title, data, cb)
+--     sendLog(title, data, cb)
+-- end)
